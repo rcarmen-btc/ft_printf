b53d2c02f8f7fa845dd9403f8220bd1b42eb0500
rcarmen <rcarmen@student.21-school.ru>
2021-01-08 23:47:44 +0300

    reload

--- dig_int_print.c
+++ dig_int_print.c
@@ -8,14 +8,24 @@ void		with_precision(t_specs *stuff, char *str)
 
 void		dig_int_print(t_specs *stuff)
 {
-	int		digit_len;
-	char	*digit_str;
-
+	int digit_len;
+	char *digit_str;
+	int i;
 	//if (!(str = (char *)malloc(sizeof(char))))
 	//	return (NULL);
+	i = 0;
 	digit_str = ft_itoa(va_arg(stuff->f_varg, int));
-	digit_len = ft_strlen(digit_str);	
-	if (stuff->flag == zero && stuff->flag != minus && stuff->point == NULL)	
+	digit_len = ft_strlen(digit_str);
+	if (stuff->width > digit_len)
+		while (i < stuff->width - digit_len)
+		{
+			ft_putchar_fd('0', 1);
+			i++;
+		}
+
+
+	to = digit_len
+	if (stuff->flag == zero && stuff->point == NULL)
 	{
 		while ()
 		{
