a254a751a834aae023bbe088109596214564483f
rcarmen <rcarmen@student.21-school.ru>
2021-01-20 20:48:47 +0300

    reload

--- main.c
+++ main.c
@@ -4,33 +4,8 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-//int main()
-//{
-//	char *tst = "%-0*.*i";
-//	int w = 0;
-//	int p = 5;
-//	int c = 433;
-//	//ft_printf("this %i number", 17);
-//	ft_printf("=%i\n", ft_printf (tst, w, p, c));
-//    printf("=%i\n",    printf (tst, w, p, c));
-//	return (0);
-//}
 int main()
 {
-    //ft_printf("%3s", "d");
-    //printf("1:%3s\n", "HelloWorld");
-    //char *c = NULL;
-    //write(1, c, 1);
-    //ft_printf("Hello |%p|\n", p);
-	//    ft_printf("%p\n", NULL);
-	unsigned long a = 232;
-	char *b = ft_itoa_base(a, 16, 0);
-	ft_printf("%-20.*p\n", -1,b);
-	printf("%-20.*p\n", -1,b);
-	//printf("Hello |%-4.3x|\n", 32);
-    //	ft_itoa_base(-19, 3);
-    //ft_printf("%s\n", ft_itoa_base(-23, 10));
-    //printf("%d\n", i);
-    //ft_printf("|%s|", "Hello");
-    //printf("%s\n", NULL);
+    int a;
+	ft_printf("%16p\n", &a);
 }
--- src/ft_perc_print.c
+++ src/ft_perc_print.c
@@ -6,7 +6,7 @@
 /*   By: rcarmen <rcarmen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 15:48:53 by rcarmen           #+#    #+#             */
-/*   Updated: 2021/01/18 15:38:00 by rcarmen          ###   ########.fr       */
+/*   Updated: 2021/01/19 16:36:16 by rcarmen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@ void		ft_perc_print(t_specs *stuff)
 {
 	char	perc;
 	int		i;
+	char	z_s;
 
 	i = 0;
 	perc = '%';
-	if (stuff->flag == minus)
+	z_s = ' ';
+	if (stuff->flag == minus || stuff->flag == (minus | zero))
 	{
 		ft_putchar_fd(perc, 1);
 		while (i++ < stuff->width - 1)
-			ft_putchar_fd(' ', 1);
+			ft_putchar_fd(z_s, 1);
+	}
+	else
+	{	
+		z_s = stuff->flag == zero ? '0' : ' ';
+		while (i++ < stuff->width - 1)
+			ft_putchar_fd(z_s, 1);
+		ft_putchar_fd(perc, 1);
 	}
 }
