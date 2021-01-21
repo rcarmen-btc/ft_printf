0d38339d5ae4e36c648eb93887dd7e582a93d37c
rcarmen <rcarmen@student.21-school.ru>
2021-01-22 00:01:18 +0300

    reload

--- src/ft_x_print.c
+++ src/ft_x_print.c
@@ -6,7 +6,7 @@
 /*   By: rcarmen <rcarmen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 15:48:53 by rcarmen           #+#    #+#             */
-/*   Updated: 2021/01/21 11:34:08 by rcarmen          ###   ########.fr       */
+/*   Updated: 2021/01/22 00:00:13 by rcarmen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,7 @@ static void			set_lenth_put_char(t_specs *stuff, const char c, int fd)
 	stuff->full_lenth++;
 }
 
-void			low_itoa_hex(t_specs *stuff, unsigned long long n)
-{
-	if (n > 15)
-	{
-		low_itoa_hex(stuff, n / 16);
-		low_itoa_hex(stuff, n % 16);
-	}
-	else
-	{
-		if (n <= 9)
-			set_lenth_put_char(stuff, n + '0', 1);
-		else
-			set_lenth_put_char(stuff, n - 10 + 'a', 1);
-	}
-}
-
-static void			ot_fl_influ(t_specs *stuff, unsigned long long ns, int nl, int d)
+static void			ot_fl_influ(t_specs *stuff, char *ns, int nl, int d)
 {
 	int				i;
 	char			z_n;
@@ -50,16 +34,20 @@ static void			ot_fl_influ(t_specs *stuff, unsigned long long ns, int nl, int d)
 			set_lenth_put_char(stuff, z_n, 1);
 	while (i++ < d)
 		set_lenth_put_char(stuff, '0', 1);
-	if (stuff->precision < 0 && ns == 0)
+	if (stuff->precision < 0 && ft_strncmp(ns, "0", nl) == 0)
 		ft_u_ptf_putnbr_fd(0, 1);
 	i = 0;
-	if (ns != 0 || stuff->point == NULL)
-		low_itoa_hex(stuff, ns);
+	if (ft_strncmp(ns, "0", nl) != 0 || stuff->point == NULL)
+		while (i < nl)
+		{
+			set_lenth_put_char(stuff, *(ns + i), 1);
+			i++;
+		}
 }
 
-static void		mi_fl_influ(t_specs *stuff, unsigned long long ns, int nl, int d)
+static void			mi_fl_influ(t_specs *stuff, char *ns, int nl, int d)
 {
-	int			i;
+	int				i;
 
 	i = 0;
 	if (stuff->flag == minus || stuff->flag == (minus | zero))
@@ -67,8 +55,12 @@ static void		mi_fl_influ(t_specs *stuff, unsigned long long ns, int nl, int d)
 		while (i++ < d)
 			set_lenth_put_char(stuff, '0', 1);
 		i = 0;
-		if (ns != 0 || stuff->point == NULL)
-			low_itoa_hex(stuff, ns);
+		if (ft_strncmp(ns, "0", nl) != 0 || stuff->point == NULL)
+			while (i < nl)
+			{
+				set_lenth_put_char(stuff, *(ns + i), 1);
+				i++;
+			}
 		while (stuff->width-- > nl + d)
 			set_lenth_put_char(stuff, ' ', 1);
 	}
@@ -76,29 +68,22 @@ static void		mi_fl_influ(t_specs *stuff, unsigned long long ns, int nl, int d)
 		ot_fl_influ(stuff, ns, nl, d);
 }
 
-void				ft_x_print(t_specs *stuff)
+void					ft_x_print(t_specs *stuff)
 {
 	int					nbr_len;
 	unsigned long long	nbr;
-	unsigned long long	tmp;
+	char				*nbr_str;
 	int					diff;
 
-	nbr_len = 0;
-	nbr = va_arg(stuff->f_varg, unsigned long);
-	tmp = nbr;
-	while (tmp > 0)
-	{
-		tmp /= 16;
-		nbr_len++;
-	}
-	if (nbr == 0 && stuff->point == NULL)
-		nbr_len = 1;
-//	if (stuff->point != NULL && nbr == 0)
-//		nbr_len--;
+	nbr = (unsigned int)(va_arg(stuff->f_varg, unsigned long long));
+	nbr_str = ft_itoa_base(nbr, 16, 0);
+	nbr_len = ft_strlen(nbr_str);
+	if (stuff->point != NULL && nbr == 0)
+		nbr_len--;
 	if (stuff->precision < 0)
 		diff = 0;
 	else
 		diff = stuff->precision - nbr_len;
 	diff = diff > 0 ? diff : 0;
-	mi_fl_influ(stuff, nbr, nbr_len, diff);
+	mi_fl_influ(stuff, nbr_str, nbr_len, diff);
 }
