b5f3b9bd23bb19180559b99adbe2b3f362160bd0
rcarmen <rcarmen@student.21-school.ru>
2021-01-21 22:35:17 +0300

    reload

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ int main()
 {
 	int a;
 	//ft_printf("ultimate2 %*d %*s %*x %*X %*i %*u\n", 1, 5000, 1, "hey", 10, 54700, 1, 300, 10000, -55, 1, -60);
-	ft_printf("%.3x\n", 0);
+	ft_printf("%4p\n", 0);
 	//ft_printf("%% *.5i 42 == |% *.5i|", 4, 42);
 }
